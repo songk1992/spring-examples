dfc8e9adfe35750995e18723af19032edc9526ba
songk1992-home <zuppiy@naver.com>
2022-05-28 23:36:05 +0900

    init

---
+++ make-custom-orm/src/main/java/com/example/makecustomorm/MakeCustomOrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.makecustomorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeCustomOrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeCustomOrmApplication.class, args);
+    }
+
+}
---
+++ make-custom-orm/src/test/java/com/example/makecustomorm/MakeCustomOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makecustomorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeCustomOrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
